3fbc8e4fb422d8c099380c345ae8e70d605ad2f8
Gabriel Bento <bielnovaes61@gmail.com>
2026-06-10 15:44:30 -0300

    adicionando fundo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
 let canvas;
 let gameContainer;
+let backgroundImg;
+let candyImg;
+let characterImg;
+
+
+function preload() {
+    backgroundImg = loadImage("img/background.jpg", () => console.log("background carregado!"), () => console.log("erro ao carregar background"));
+    candyImg = loadImage("img/candy.png");
+    characterImg = loadImage("img/character.png");
+
+}
 
 function setup() {
-    canvas = createCanvas(500, 700);
+    canvas = createCanvas(960, 540);
     canvas.parent("game-container");
     gameContainer = document.getElementById("game-container");
+
+    console.log(backgroundImg);
+}
+
+function draW() {
+    background(255, 0, 0);
+
+    imageMode(CORNER);
+
+    image(backgroundImg, 0, 0, width, height);
+    Engine.update(engine);
+    
 }
